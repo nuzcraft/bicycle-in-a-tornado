ba8e999ce2ff9200dade8c951dfae55c313a3f82
nuzcraft <nuzcraft@gmail.com>
2023-01-08 06:23:33 -0600

    clamping bicycle to stay on the screen

--- scenes/gameplay/gameplay.gd
+++ scenes/gameplay/gameplay.gd
@@ -1,6 +1,13 @@
 extends Node
 
-var elapsed = 0
+onready var camera_pos = $Camera2D.global_position
+onready var camera_bounds = {
+	"top": camera_pos.y - Game.size.y / 2,
+	"bottom": camera_pos.y + Game.size.y / 2,
+	"left": camera_pos.x - Game.size.x / 2,
+	"right": camera_pos.x + Game.size.x / 2,
+}
+onready var bicycle = $Bicycle
 
 # `pre_start()` is called when a scene is loaded.
 # Use this function to receive params from `Game.change_scene(params)`.
@@ -24,4 +31,10 @@ func start():
 
 
 func _process(delta):
-	pass
+	bicycle.global_position.x = clamp(bicycle.global_position.x, camera_bounds["left"], camera_bounds["right"])
+	if bicycle.global_position.x <= camera_bounds["left"] or bicycle.global_position.x >= camera_bounds["right"]:
+		bicycle.velocity.x = 0
+		
+	bicycle.global_position.y = clamp(bicycle.global_position.y, camera_bounds["top"], camera_bounds["bottom"])
+	if bicycle.global_position.y <= camera_bounds["top"] or bicycle.global_position.y >= camera_bounds["bottom"]:
+		bicycle.velocity.y = 0
